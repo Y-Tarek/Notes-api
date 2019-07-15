8e2909dbb640961762a047eb03ebafde04d1d480
yassertarek <yasser.tarek@uflare.io>
2019-07-15 14:15:50 +0200

    get-Tests

--- server/server.js
+++ server/server.js
@@ -22,8 +22,18 @@ app.post('/todos',(req,res) => {
 
 });
 
+app.get('/todos',(req,res) => {
+     Todo.find().then((todos) => {
+         res.send({todos:todos});
+     },(e) => {
+         console.log(e);
+     })
+})
+
 
 app.listen(3000,()=>{
     console.log('Server Started');
-})
+});
+
+module.exports = {app:app};
  
